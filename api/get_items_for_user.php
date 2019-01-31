2a47a3bfabeb66675ffa8ae31f50dcb0b2a7f007
Jeffrey Thor <thorjeffrey@gmail.com>
2019-01-31 12:06:37 -0500

    Adds get items for user endpoint

---
+++ api/get_items_for_user.php
@@ -0,0 +1,30 @@
+<?php
+include_once 'database.php';
+
+$user_id = $_POST['user_id'];
+
+if ($user_id == '') {
+    $dataArray = array('success' => false, 'error' => 'no user id provided');
+    header('Content-Type: application/json');
+    die(json_encode($dataArray));
+}
+
+$user_id = strip_tags($user_id);
+
+$password = hash('sha256', $upass);
+
+$query = "SELECT item_id, amount FROM LOA.t_user_item WHERE user_id = '$user_id'";
+$dataArray = array();
+
+if ($result = mysqli_query($link, $query)) {
+  // Fetch one and one row
+  while ($row = mysqli_fetch_row($result)) {
+  	$dataArray[] = array('item_id' => $row[0], 'amount' => $row[1]);
+  }
+  // Free result set
+  mysqli_free_result($result);
+}
+
+header('Content-Type: application/json');
+echo json_encode($dataArray);
+?>
