6f3ce057c1168953308c42f1384015b61e47cb4b
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-08 18:51:42 -0500

    logout all users request

---
+++ api/logout_all_users.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'database.php';
+
+$query = "UPDATE LOA.t_user SET logged_in = 0 WHERE logged_in = 1";
+
+if ($result = mysqli_query($link, $query)) {
+    $dataArray = array('success' => true, 'error' => '');
+} else {
+    $dataArray = array('success' => false, 'error' => 'Something went wrong');
+}
+
+header('Content-Type: application/json');
+echo json_encode($dataArray);
+?>
