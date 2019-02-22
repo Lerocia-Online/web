4366217ecd22da250f9eb6b8c0b3c01b76fb672d
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-21 20:07:29 -0500

    Add set user transform endpoint

---
+++ api/set_transform_for_user.php
@@ -0,0 +1,41 @@
+<?php
+include_once 'database.php';
+
+$user_id = $_POST['user_id'];
+$position_x = $_POST['position_x'];
+$position_y = $_POST['position_y'];
+$position_z = $_POST['position_z'];
+$rotation_x = $_POST['rotation_x'];
+$rotation_y = $_POST['rotation_y'];
+$rotation_z = $_POST['rotation_z'];
+
+if ($user_id == '') {
+    $dataArray = array('success' => false, 'error' => 'must provide user id');
+    header('Content-Type: application/json');
+    die(json_encode($dataArray));
+}
+
+$user_id = strip_tags($user_id);
+
+$query = "
+UPDATE LOA.t_user
+  SET 
+      position_x = '$position_x', 
+      position_y = '$position_y', 
+      position_z = '$position_z', 
+      rotation_x = '$rotation_x', 
+      rotation_y = '$rotation_y', 
+      rotation_z = '$rotation_z'
+WHERE user_id = '$user_id'
+";
+
+$dataArray = array();
+if ($result = mysqli_query($link, $query)) {
+    $dataArray = array('success' => true, 'error' => '');
+} else {
+    $dataArray = array('success' => false, 'error' => 'Could not update transform');
+}
+
+header('Content-Type: application/json');
+echo json_encode($dataArray);
+?>
