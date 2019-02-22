3f47d55bcb133c225ec8f323c19b804508860aee
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-21 21:26:56 -0500

    Adds equip weapon and armor endpoints

---
+++ api/set_equipped_weapon_for_user.php
@@ -0,0 +1,30 @@
+<?php
+include_once 'database.php';
+
+$user_id = $_POST['user_id'];
+$weapon_id = $_POST['weapon_id'];
+
+if ($user_id == '' || $weapon_id) {
+    $dataArray = array('success' => false, 'error' => 'must provide user id and weapon id');
+    header('Content-Type: application/json');
+    die(json_encode($dataArray));
+}
+
+$user_id = strip_tags($user_id);
+
+$query = "
+UPDATE LOA.t_user
+  SET equipped_weapon = '$weapon_id'
+WHERE user_id = '$user_id'
+";
+
+$dataArray = array();
+if ($result = mysqli_query($link, $query)) {
+    $dataArray = array('success' => true, 'error' => '');
+} else {
+    $dataArray = array('success' => false, 'error' => 'Could not update equipped weapon');
+}
+
+header('Content-Type: application/json');
+echo json_encode($dataArray);
+?>
