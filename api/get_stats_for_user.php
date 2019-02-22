90bd1d8eb382ba7e56936240dde5fdd977bdc9e2
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-22 16:50:57 -0500

    Adds health, stamina, and gold to get stats for user

--- api/get_stats_for_user.php
+++ api/get_stats_for_user.php
@@ -22,8 +22,13 @@ SELECT
        rotation_y, 
        rotation_z, 
        type, 
+       max_health,
+       current_health,
+       max_stamina,
+       current_stamina,
+       gold,
        equipped_weapon, 
-       equipped_armor 
+       equipped_apparel 
 FROM LOA.t_user 
 WHERE user_id = '$user_id'
 ";
@@ -32,19 +37,24 @@ $row = mysqli_fetch_row($result);
 $dataArray = array();
 if ($row) {
     $dataArray = array(
-        'success'         => true,
-        'error'           => '',
-        'user_id'         => "$row[0]",
-        'username'        => "$row[1]",
-        'position_x'      => "$row[2]",
-        'position_y'      => "$row[3]",
-        'position_z'      => "$row[4]",
-        'rotation_x'      => "$row[5]",
-        'rotation_y'      => "$row[6]",
-        'rotation_z'      => "$row[7]",
-        'type'            => "$row[8]",
-        'equipped_weapon' => "$row[9]",
-        'equipped_armor'  => "$row[10]"
+        'success'          => true,
+        'error'            => '',
+        'user_id'          => "$row[0]",
+        'username'         => "$row[1]",
+        'position_x'       => "$row[2]",
+        'position_y'       => "$row[3]",
+        'position_z'       => "$row[4]",
+        'rotation_x'       => "$row[5]",
+        'rotation_y'       => "$row[6]",
+        'rotation_z'       => "$row[7]",
+        'type'             => "$row[8]",
+        'max_health'       => "$row[9]",
+        'current_health'   => "$row[10]",
+        'max_stamina'      => "$row[11]",
+        'current_stamina'  => "$row[12]",
+        'gold'             => "$row[13]",
+        'equipped_weapon'  => "$row[14]",
+        'equipped_apparel' => "$row[15]"
     );
 } else {
     $dataArray = array('success' => false, 'error' => 'Something went wrong');
