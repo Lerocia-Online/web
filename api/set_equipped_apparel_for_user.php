2bca65fdb3dbf71b0efaa6d2df9a92551ed62952
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-22 15:38:44 -0500

    Fixes armor to be named apparel

--- api/set_equipped_apparel_for_user.php
+++ api/set_equipped_apparel_for_user.php
@@ -2,10 +2,10 @@
 include_once 'database.php';
 
 $user_id = $_POST['user_id'];
-$armor_id = $_POST['armor_id'];
+$apparel_id = $_POST['apparel_id'];
 
-if ($user_id == '' || $armor_id == '') {
-    $dataArray = array('success' => false, 'error' => 'must provide user id and armor id');
+if ($user_id == '' || $apparel_id == '') {
+    $dataArray = array('success' => false, 'error' => 'must provide user id and apparel id');
     header('Content-Type: application/json');
     die(json_encode($dataArray));
 }
@@ -14,7 +14,7 @@ $user_id = strip_tags($user_id);
 
 $query = "
 UPDATE LOA.t_user
-  SET equipped_armor = '$armor_id'
+  SET equipped_apparel = '$apparel_id'
 WHERE user_id = '$user_id'
 ";
 
@@ -22,7 +22,7 @@ $dataArray = array();
 if ($result = mysqli_query($link, $query)) {
     $dataArray = array('success' => true, 'error' => '');
 } else {
-    $dataArray = array('success' => false, 'error' => 'Could not update equipped armor');
+    $dataArray = array('success' => false, 'error' => 'Could not update equipped apparel');
 }
 
 header('Content-Type: application/json');
