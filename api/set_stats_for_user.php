6412b15c18ebeabb4980aa6cbbff494b111d09bb
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-22 17:44:36 -0500

    Updates set transform for user to set stats for user

--- api/set_stats_for_user.php
+++ api/set_stats_for_user.php
@@ -8,6 +8,14 @@ $position_z = $_POST['position_z'];
 $rotation_x = $_POST['rotation_x'];
 $rotation_y = $_POST['rotation_y'];
 $rotation_z = $_POST['rotation_z'];
+$type = $_POST['type'];
+$max_health = $_POST['max_health'];
+$current_health = $_POST['current_health'];
+$max_stamina = $_POST['max_stamina'];
+$current_stamina = $_POST['current_stamina'];
+$gold = $_POST['gold'];
+$equipped_weapon = $_POST['equipped_weapon'];
+$equipped_apparel= $_POST['equipped_apparel'];
 
 if ($user_id == '') {
     $dataArray = array('success' => false, 'error' => 'must provide user id');
@@ -32,7 +40,15 @@ UPDATE LOA.t_user
       position_z = '$position_z', 
       rotation_x = '$rotation_x', 
       rotation_y = '$rotation_y', 
-      rotation_z = '$rotation_z'
+      rotation_z = '$rotation_z',
+      type = '$type',
+      max_health = '$max_health',
+      current_health = '$current_health',
+      max_stamina = '$max_stamina',
+      current_stamina = '$current_stamina',
+      gold = '$gold',
+      equipped_weapon = '$equipped_weapon',
+      equipped_apparel = '$equipped_apparel'
 WHERE user_id = '$user_id'
 ";
 
@@ -40,7 +56,7 @@ $dataArray = array();
 if ($result = mysqli_query($link, $query)) {
     $dataArray = array('success' => true, 'error' => '');
 } else {
-    $dataArray = array('success' => false, 'error' => 'Could not update transform');
+    $dataArray = array('success' => false, 'error' => 'Could not update stats');
 }
 
 header('Content-Type: application/json');
