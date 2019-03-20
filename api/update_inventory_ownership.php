283e9d77363cce6289afb083507bb6fff857d39a
Jeffrey Thor <thorjeffrey@gmail.com>
2019-03-19 22:47:43 -0400

    Adds query to delete old inventory when changing inventory ownership

--- api/update_inventory_ownership.php
+++ api/update_inventory_ownership.php
@@ -13,13 +13,18 @@ if ($original_owner_id == '' || $new_owner_id == '') {
 $original_owner_id = strip_tags($original_owner_id);
 $new_owner_id = strip_tags($new_owner_id);
 
-$query = "UPDATE LOA.t_character_item SET character_id = '$new_owner_id' WHERE character_id = '$original_owner_id'";
+$delete_query = "DELETE FROM LOA.t_character_item WHERE character_id = '$new_owner_id'";
 
 $dataArray = array();
-if (mysqli_query($link, $query)) {
-    $dataArray = array('success' => true, 'error' => '');
+if (mysqli_query($link, $delete_query)) {
+    $update_query = "UPDATE LOA.t_character_item SET character_id = '$new_owner_id' WHERE character_id = '$original_owner_id'";
+    if (mysqli_query($link, $update_query)) {
+        $dataArray = array('success' => true, 'error' => '');
+    } else {
+        $dataArray = array('success' => false, 'error' => 'could not update inventory ownership');
+    }
 } else {
-    $dataArray = array('success' => false, 'error' => 'Could not update inventory ownership');
+    $dataArray = array('success' => false, 'error' => 'Could not delete old inventory');
 }
 
 header('Content-Type: application/json');
